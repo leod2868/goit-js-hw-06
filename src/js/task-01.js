19c20f0155b036f42e00e4ba8f9f7ee32343aa15
Leonid <ztvlnd777@gmail.com>
2023-02-17 23:03:33 +0200

    2.5

---
+++ src/js/task-01.js
@@ -0,0 +1,16 @@
+const megaCategories = document.querySelector('#categories');
+
+const bigliEl = document.querySelectorAll('.item');
+
+console.log('Number of categories:', bigliEl.length);
+
+
+bigliEl.forEach((liel) => {
+    const y = document.querySelector('.item > h2 ')
+    const x = document.querySelectorAll( '.item > li ')
+    console.log('Category:', y.textContent)
+    console.log('Elements:', x.length  )
+}
+
+)
+  
