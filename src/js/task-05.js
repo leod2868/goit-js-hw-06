80c2f103154839881fc1bde6380e1b6cbe8d4c71
Leonid <ztvlnd777@gmail.com>
2023-02-20 18:20:12 +0200

    4.5

---
+++ src/js/task-05.js
@@ -0,0 +1,13 @@
+const refs = {
+
+    input: document.querySelector('#name-input'),
+    nameLabel: document.querySelector('#name-output'),   
+}
+
+refs.input.addEventListener('input', onInputChange)
+
+function onInputChange(event) {
+
+    if (event.currentTarget.value === "") { refs.nameLabel.textContent = 'Anonymous' };
+    refs.nameLabel.textContent = event.currentTarget.value;
+}
